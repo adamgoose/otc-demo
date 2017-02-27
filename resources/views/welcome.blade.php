f7bf8ca329b2ff17c7672465e177e4a2ef68ca1f
Adam Engebretson <adam.engebretson@bridgeconx.com>
2017-02-27 17:58:18 -0600

    Super Awesome Commit!
    This is a really important change to the source code!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    OTC IS AMAZBALLZ
                 </div>
 
                 <div class="links">
