6fbc0d6fe2ae184612a0c60361bf71ddfee07100
Adam Engebretson <adam.engebretson@bridgeconx.com>
2017-02-27 18:11:25 -0600

    canging vopty

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    OTC IS AMAZBALLZz
+                    OTC IS Super amazing!
                 </div>
 
                 <div class="links">
